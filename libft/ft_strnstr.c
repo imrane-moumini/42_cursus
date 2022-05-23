f1b8cf002ef3cf01c6f9193d84509044e7e70f35
Imrane MOUMINI <imoumini@e3r7p2.clusters.42paris.fr>
2022-05-23 23:37:05 +0200

    strnstr add

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -9,6 +9,7 @@
 /*   Updated: 2022/05/22 22:56:46 by imoumini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 #include <stdio.h>
 #include <bsd/string.h>
@@ -17,45 +18,34 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	lenght;
 	size_t	save;
 
 	i = 0;
 	j = 0;
-	lenght = ft_strlen(little);
-	if (lenght == 0)
-	{
+	if (ft_strlen(little) == 0)
 		return ((char *)(big));
-	}
-	while (((big[i] != '\0') && (big[j] != '\0')) && (i < len))
+	while (((big[i] != '\0') && (little[j] != '\0')) && (i < len))
 	{
-		j = 0;
-		while ((big[i] == little[j]) && (i < len))
+		save = i;
+		while (big[i] == little[j] && (i < len))
 		{
-			while ((big[i] == little[j]) && (i < len))
-			{
-				j++;
-				save = i;
-				if ((j == lenght)/* && (big[i] == little[j])*/)
-				{
-					j--;	
-					save = save - j;
-					return ((char *)(&(big[save])));
-				}
-				i++;
-			}
-			i--;
+			i++;
+			j++;
+			if (j == ft_strlen(little))
+				return ((char *)(&(big[i - j])));
 		}
-		i++;
-
+		j = 0;
+		i = save + 1;
 	}
 	return (NULL);
 }
-/*int	main(void)
+
+/*
+int	main(void)
 {
 	char haystack[30] = "aaabcabcd";
-	char needle[10] = "aabc";
+	char needle[10] = "cd";
 	
-	printf("mine is =>%s\n", ft_strnstr("lorem ipsum dolor sit amet", "ipsumm", 30));
-	printf("the real one is =>%s", strnstr("lorem ipsum dolor sit amet", "ipsumm", 30));
+	printf("mine is =>%s\n", ft_strnstr(haystack, needle, 8));
+	printf("the real one is =>%s", strnstr(haystack, needle, 8));
 }*/
