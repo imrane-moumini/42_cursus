ff9468878a9865ab83f09769f7a6f096d0e5a9b1
Imrane MOUMINI <imoumini@e3r3p1.clusters.42paris.fr>
2022-05-31 23:12:40 +0200

    add ft_split not finish

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -15,51 +15,99 @@
 
 int number_of_tab(char const *s, char c)
 {
-    int i;
-    int count;
+	int i;
+	int count;
 
-    i = 0;
-    count = 0;
-    while (s[i] != '\0')
-    {
-        if (s[i] == c)
-        {
-            count++;
-        }
-        i++;
-    }
-    return (count);
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			count++;
+		}
+		i++;
+	}
+	return (count + 1);
 } 
-
-int size_of_a_tab(char const *s, char c)
+void	fill_tab(char const *s, char c, char **p)
 {
-    int i;
-    int count;
+	int	i;
+	int j;
+	int	k;
 
-    i = 0;
-    count = 0;
-    while (s[i] != '\0')
-    {
-        if (s[i] != c)
-        {
-            count++;
-        }
-        if (s[i] == c)
-        {
-            return(count);
-        }
-        i++;
-    }
-    return (count);
+	j = 0;
+	i = 0;
+	k = 0;
+
+	while (s[i] != '\0')
+	{
+		while ((s[i] != c) && (s[i] != '\0'))
+		{
+			p[j][k] = s[i];
+			k++;
+			i++;
+		}
+		p[j][k] = '\0';
+		if (s[i] == c)
+		{
+			k = 0;
+			j++;
+		}
+		i++;
+	}
 }
+
 char    **ft_split(char const *s, char c)
 {
-    int i;
-    
-    i = 0;
+	int i;
+	int j;
+	int	number_of_tabs;
+	int count;
+	int lenght;
+	
+	number_of_tabs = number_of_tab(s,c);
+	i = 0;
+	j = 0;
+	count = 0;
+	// creer un pointeur qui enreistre l'adresse memoire d'un autre pointeur de type char (une collection d eppinteur en loccurence)
+	char **p;
+	// definir la taille de cette collection depointeur (le nbr de pointeur present dans ce tableau de pointeur)
+	p = (char **)malloc(sizeof(char *) * number_of_tabs);
+	// definir le nbr delement de caque tableau individuellement
+	lenght = ft_strlen(s);
+	while (i <= lenght)
+	{
+		if (s[i] == c || i == lenght)
+		{
+			p[j] = malloc((char) sizeof(char) *count + 1);
+			j++;
+			count = 0;
+		}
+		if (s[i] != c && i != lenght)
+	        {
+	          count++;
+	        }
+		i++;
+	}
+	fill_tab(s,c, p);
+	return (p);
 
 }
-int main(void)
+/*int main(void)
 {
-    printf("%s", ft_split("je suis bg", ' '));
-}
+	int i;
+	char s[] = "je suis un bg";
+	char c = ' ';
+	char **p;
+
+	i = 0;
+	p = ft_split(s, c);
+
+	while (i < number_of_tab(s,c))
+	{
+		printf("%s\n", p[i]);
+		i++;
+	}
+
+}*/
