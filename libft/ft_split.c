54f7df174bd88a5af79e2463c789ca93ce0d74ab
Imrane MOUMINI <imoumini@e3r10p5.clusters.42paris.fr>
2022-06-04 00:17:14 +0200

    split finish

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -14,14 +14,13 @@
 #include <unistd.h>
 #include "libft.h"
 
-int number_of_tab(char const *s, char c)
+int	number_of_tab(char const *s, char c)
 {
-	int i;
-	int count;
+	int	i;
+	int	count;
 
 	i = 0;
 	count = 0;
-
 	while (s[i] != '\0')
 	{
 		if ((s[i] != c) && (s[i] != '\0'))
@@ -30,144 +29,87 @@ int number_of_tab(char const *s, char c)
 			i++;
 		}
 		while ((s[i] != c) && (s[i] != '\0'))
-		{
 			i++;
-		}
 		while ((s[i] == c) && (s[i] != '\0'))
-		{
 			i++;
-		}
 	}
-	printf("count %d\n", count);
 	return (count);
-} 
+}
+
 void	fill_tab(char const *s, char c, char **p, int l)
 {
 	int	i;
-	int j;
+	int	j;
 	int	k;
 
 	j = 0;
 	i = 0;
 	k = 0;
-
 	while (s[i] != '\0')
 	{
-		while ((s[i] == c) && (i <= l))
-		{
+		while ((s[i] == c) && (i <= l - 1))
 			i++;
-		}
-		while ((s[i] != c) && (i <= l))
-		{
-			p[j][k] = s[i];
-			k++;
-			i++;
-		}
-		p[j][k] = '\0';
-		if (s[i] == c)
+		while ((s[i] != c) && (i <= l - 1))
+			p[j][k++] = s[i++];
+		if (i <= l - 1)
+			p[j][k] = '\0';
+		if (i <= l - 1)
 		{
 			k = 0;
 			j++;
+			i++;
 		}
-		i++;
 	}
-	j++;
-	p[j] = NULL;
 }
 
-char    **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	int i;
-	int j;
-	int	number_of_tabs;
-	int count;
-	int lenght;
-	printf("test\n");
-	lenght = ft_strlen(s);
-	number_of_tabs = number_of_tab(s,c);
+	size_t	i;
+	int		j;
+	int		count;
+	char	**p;
+
 	i = 0;
 	j = 0;
 	count = 0;
-	// creer un pointeur qui enreistre l'adresse memoire d'un autre pointeur de type char (une collection d eppinteur en loccurence)
-	char **p;
-	// definir la taille de cette collection depointeur (le nbr de pointeur present dans ce tableau de pointeur)
-	p = (char **)malloc(sizeof(char *) * number_of_tabs + 1);
-	if (number_of_tabs == 0)
-	{
-		p = NULL;
+	p = (char **)ft_calloc(sizeof(char *), (number_of_tab(s, c) + 1));
+	if (number_of_tab(s, c) == 0)
 		return (p);
-	}
-	// definir le nbr delement de caque tableau individuellement
-	while (i <= lenght - 1)
+	while (i <= (ft_strlen(s) - 1))
 	{
-		while (((s[i] == c) && (i <= lenght - 1)))
-		{
+		while ((s[i] == c) && (s[i]))
 			i++;
-		}
-		while ((s[i] != c) && (i <= lenght -1))
-		{
+		while ((s[i] != c) && (s[i++]))
 			count++;
-			i++;
-		}
-		printf("%d\n", count);
-		p[j] = malloc((char) sizeof(char) *count + 1);
-		j++;
+		if (count != 0)
+			p[j++] = ft_calloc((char) sizeof(char), (count + 1));
 		count = 0;
 		i++;
 	}
-	fill_tab(s,c, p, lenght);
+	fill_tab(s, c, p, ft_strlen(s));
 	return (p);
 }
 
-int		main(void)
-{
-	char	**tabstr;
-	int		i;
-	
-
-	i = 0;
-	if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ')))
-		printf("NULL");
-	else
-	{
-		while (tabstr[i] != NULL)
-		{
-			printf("%s\n", tabstr[i]);
-			write(1, "\n", 1);
-			i++;
-		}
-	}
-}
-
-// int main(void)
-
+// int        main(void)
 // {
-// 	int i;
-// 	char s[] = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
-// 	char c = ' ';
-// 	char **p;
-	
-// 	i = 0;
-// 	p = ft_split(s, c);
+//     char    **tabstr;
+//     // int        i;
+//     tabstr = ft_split("  tripouille  42  ", ' ');
 
-// 	while (i <= number_of_tab(s,c))
-// 	{
-// 		//printf("%s\n", p[i]);
-// 		i++;
-// 	}
-// 	i = 0;
-// 	while (i <= number_of_tab(s,c))
-// 	{
-// 		if (p[12] == NULL)
-// 		{
-// 			printf("TRUE\n");
-// 		}
-// 		else
-// 		{
-// 			printf("FALSE\n");
-// 		}
-// 		//printf("%s", p[1]);
-// 		i++;
-// 	}
-
+//     // i = 0;
+//     if (tabstr[2] == NULL)
+//         printf("NULL\n");
+// }
+// 	return 0;
+	//  (!(tabstr = ft_split("  tripouille  42  ", ' ')))
+	// 	printf("NULL");
+	// else
+	// {
+	// 	while (tabstr[i] != NULL)
+	// 	{
+	// 		printf("%s\n", tabstr[i]);
+	// 		//write(1, "\n", 1);
+	// 		i++;
+	// 	}
+	// }
 // }
