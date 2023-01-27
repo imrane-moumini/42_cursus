4973f576f69416866ddec844ec130e2dde79fd0c
Imrane MOUMINI <imoumini@e2r9p10.clusters.42paris.fr>
2023-01-27 20:59:48 +0100

    fix one philo

--- philosophers/philo/error.c
+++ philosophers/philo/error.c
@@ -6,7 +6,7 @@
 /*   By: imoumini <imoumini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 15:57:34 by imoumini          #+#    #+#             */
-/*   Updated: 2023/01/27 19:08:30 by imoumini         ###   ########.fr       */
+/*   Updated: 2023/01/27 20:58:37 by imoumini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,7 @@ int	handle_second_error(int argc, char *argv[])
 	if (ft_atoi_parsing(argv[1]) == 1)
 	{
 		printf("0 1 has taken a fork\n");
+		usleep(ft_atoi_parsing(argv[2]) * 1000);
 		printf("%i 1 died\n", ft_atoi_parsing(argv[2]));
 		return (0);
 	}
