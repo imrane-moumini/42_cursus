0833e7dd3a7a4b70f7ecd476239b75e0dbf5e7e0
imrane-moumini <imranemouminiprogmail.com>
2023-02-04 19:33:02 +0100

    ajout fonction manipulation input

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -11,11 +11,11 @@ typedef struct source_s
 {   
     char *buffer;       /* the input text */
     long bufsize;       /* size of the input text */
-    long  curpos;       /* absolute char position in source */
+    long  curpos;       /* index position in source */
 } t_source;
-char next_char(t_source *src);
-void unget_char(t_source *src);
-char peek_char(t_source *src);
+char next_char(t_source *src); // return the next char et avance de 1 car dans linput (update pos)
+void unget_char(t_source *src); // recule de 1 car dans linput
+char peek_char(t_source *src); // return le next char sans update pos 
 void skip_white_spaces(t_source *src);
 
 #endif
---
+++ minishell/parsing/lexer.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imrane <imrane@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 18:35:48 by imrane            #+#    #+#             */
+/*   Updated: 2023/02/04 19:31:38 by imrane           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+char next_char(t_source *src)
+{
+    char nc;
+
+    nc = '\n';
+    if(!src || !src->buffer)
+        return (nc);
+    if (src -> buffer[src -> curpos])
+    {
+        nc = src -> buffer[src -> curpos + 1];
+        src -> curpos++;
+        return (nc);
+    }
+	return (nc);
+}
+
+void unget_char(t_source *src)
+{
+	if(!src || !src->buffer)
+        return ;
+	if (src -> curpos == 0)
+		return ;
+	else
+		src -> curpos--;
+}
+
+char peek_char(t_source *src)
+{
+    char nc;
+
+    nc = '\n';
+    if(!src || !src->buffer)
+        return (nc);
+    if (src -> buffer[src -> curpos])
+    {
+        nc = src -> buffer[src -> curpos + 1];
+        return (nc);
+    }
+	return (nc);
+}
+
+void skip_white_spaces(t_source *src)
+{
+	if(!src || !src->buffer)
+        return ;
+	while (src -> buffer[src -> curpos] && (src -> buffer[src -> curpos] == ' ' ||src -> buffer[src -> curpos] == '\t'))
+        src -> curpos++;
+}
