3a9fc42527a63fad5705f367813065337ce5d08c
imrane-moumini <imranemouminiprogmail.com>
2023-03-09 18:12:07 +0100

    fix << logic mais suelement une fois, il fautr que je le fassse pour tout l'input

--- minishell/parsing/parser/in_out.c
+++ minishell/parsing/parser/in_out.c
@@ -6,7 +6,7 @@
 /*   By: imrane <imrane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 19:17:23 by imrane            #+#    #+#             */
-/*   Updated: 2023/03/09 17:38:50 by imrane           ###   ########.fr       */
+/*   Updated: 2023/03/09 18:10:07 by imrane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,50 +30,34 @@ int ft_in_file(t_node *head)
     t_node *ptr;
     
 	ptr = head;
-	printf("c1\n");
     if (!ptr)
 		return(2);
-	printf("c2\n");
 	if (ptr -> first_child)
 		ptr = ptr -> first_child;
 	while (ptr != NULL)
 	{
-		printf("c3\n");
 		if (ft_stcmp(ptr -> txt, "<") == 1)
-		{
-			printf("c4\n");
 			break;
-		}
 		ptr = ptr -> next_sibling;
-		printf("c5\n");
 	}
 	// non concerné
 	if (ptr == NULL)
 		return (2);
-	printf("c6\n");
 	// ce que j'ai pas le droit de faire
 	if (ptr -> next_sibling)
 	{
-		printf("c7\n");
 		// si ya  <<
 		if (ft_stcmp(ptr -> next_sibling -> txt, "<") == 1)
 		{
-			printf("c8\n");
 			if (ptr -> next_sibling -> next_sibling == NULL)
 			{
-				printf("c9\n");
 				ft_printf("syntax error near unexpected token `newline'\n");
 				return (0);
 			}
 			if (ptr -> next_sibling -> next_sibling)
 			{
-				printf("c10\n");
 				if (ft_stcmp(ptr -> next_sibling -> next_sibling -> txt, "<") == 1 || ft_stcmp(ptr -> next_sibling -> next_sibling -> txt, ">") == 1)
 				{
-					printf("ptr -> txt : %s\n", ptr -> txt);
-					printf("ptr -> txt next : %s\n", ptr -> next_sibling -> txt);
-					printf("ptr -> txt next next: %s\n", ptr -> next_sibling -> next_sibling -> txt);
-					printf("c11\n");
 					ft_printf("syntax error near unexpected token '%s'\n", ptr -> next_sibling -> next_sibling -> txt);
 					return (0);
 				}
@@ -81,11 +65,21 @@ int ft_in_file(t_node *head)
 			// ne pas expand
 		}
 	}
-	else // si ya rien après le <
+	else // si ya un seul < et ya rien après le <
 	{
-		printf("c12\n");
 		ft_printf("syntax error near unexpected token `newline'\n");
 		return (0);
+	}
+	if (ptr) // si ya un seul <
+	{
+		if (ptr -> next_sibling) // si ya un truc après le <
+		{
+			if (ft_stcmp(ptr -> next_sibling -> txt, ">") == 1 || ft_stcmp(ptr -> next_sibling -> next_sibling -> txt, ">") == 1)
+				{
+					ft_printf("syntax error near unexpected token '%s'\n", ptr -> next_sibling -> txt);
+					return (0);
+				}
+		}
 	}
 	
 	// pas de pb
