263b6cca1561d7d2760fe2eedb874f742bc757d6
imrane-moumini <imranemouminiprogmail.com>
2023-03-11 17:50:11 +0100

    add ctrl + c logic, it works

--- minishell/signaux/signal.c
+++ minishell/signaux/signal.c
@@ -6,7 +6,7 @@
 /*   By: imrane <imrane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 18:26:21 by imrane            #+#    #+#             */
-/*   Updated: 2023/03/10 23:35:39 by imrane           ###   ########.fr       */
+/*   Updated: 2023/03/11 17:48:50 by imrane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ft_sigint(int sigint, siginfo_t *pid, void *idontknow)
 	(void) idontknow;
 	(void) sigint;
 	(void) pid;
+	write(1, "\n", 2);
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
