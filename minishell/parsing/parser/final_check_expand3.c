d117b73b48e4b0f0c2a2232edf3609e70be447c8
Imrane MOUMINI <imoumini@e2r9p12.clusters.42paris.fr>
2023-04-06 19:41:05 +0200

    continue continue decoupe long fonction

--- minishell/parsing/parser/final_check_expand3.c
+++ minishell/parsing/parser/final_check_expand3.c
@@ -6,7 +6,7 @@
 /*   By: imoumini <imoumini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 17:47:28 by imoumini          #+#    #+#             */
-/*   Updated: 2023/04/06 17:48:05 by imoumini         ###   ########.fr       */
+/*   Updated: 2023/04/06 19:40:27 by imoumini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,66 +18,46 @@ char **tab_without_space(char *str, int nbr)
 	char **tab;
 	int begin;
 	int end;
+	
+	begin = 0;
+	end = 0;
+	tab = malloc(sizeof(char *) * (nbr + 1 + 1));
+	tab[0] = NULL;
+	tab = tab_without_space_while(str, begin, end, tab);
+	return (tab);
+}
+char **tab_without_space_while(char *str, int begin, int end, char **tab)
+{
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
-	end = 0;
-	tab = malloc(sizeof(char *) * (nbr + 1 + 1));
 	while (str[i] != '\0')
 	{
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '-' )
 		{
 			begin = i;
 			if(begin != end)
-			{
-				tab[j] = return_right_tab(str, begin, end);
-				j++;
-			}
+				tab[j++] = return_right_tab(str, begin, end);
 			i++;
 			while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '-')
 				i++;
 			end = i;
 			if(begin != end)
-			{
-				tab[j] = return_right_tab(str, begin, end);
-				j++;
-			}
+				tab[j++] = return_right_tab(str, begin, end);
 		}
 		if (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '-' )
 			i++;
 	}
-	tab[j] = NULL;
 	return (tab);
 }
 
-char **recreate_tab_without_space(char **tab)
+char **recreate_tab_without_space_while(char **tab, char **new_tab, int i, int j)
 {
-	int i;
-	int count;
-	int tab_i;
-	int j;
-	char **new_tab;
 	char **broken_tab;
+	int tab_i;
 	
-	i = 0;
-	j = 0;
-	count = 0;
-	while (tab[i])
-	{
-		if(tab_as_space(tab[i]) == 1)
-		{
-			j--;
-			count = nbr_space(tab[i]) + count;
-		}
-		i++;
-		j++;
-	}
-	new_tab = malloc(sizeof(char *) *(count + 1 + j + 1));
-	i = 0;
-	j = 0;
-	count = 0;
 	while (tab[i])
 	{
 		if(tab_as_space(tab[i]) == 1)
@@ -99,6 +79,34 @@ char **recreate_tab_without_space(char **tab)
 		j++;
 	}
 	new_tab[j - 1] = NULL;
-	
 	return (new_tab);
+}
+
+
+char **recreate_tab_without_space(char **tab)
+{
+	int i;
+	int count;
+	int j;
+	char **new_tab;
+	
+	
+	i = 0;
+	j = 0;
+	count = 0;
+	while (tab[i])
+	{
+		if(tab_as_space(tab[i]) == 1)
+		{
+			j--;
+			count = nbr_space(tab[i]) + count;
+		}
+		i++;
+		j++;
+	}
+	new_tab = malloc(sizeof(char *) *(count + 1 + j + 1));
+	new_tab[1] = NULL; 
+	i = 0;
+	j = 0;
+	return (recreate_tab_without_space_while(tab, new_tab, i, j));
 }
