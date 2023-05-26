55437720316155e4e9f40b94dd3bdb80dd3fe60a
Imrane MOUMINI <imoumini@e2r9p11.clusters.42paris.fr>
2023-05-26 17:06:40 +0200

    fix issues draw line negatif

--- cub3d/main.c
+++ cub3d/main.c
@@ -6,7 +6,7 @@
 /*   By: imoumini <imoumini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 15:59:05 by imoumini          #+#    #+#             */
-/*   Updated: 2023/05/25 22:51:48 by imoumini         ###   ########.fr       */
+/*   Updated: 2023/05/26 17:05:05 by imoumini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,9 +62,10 @@ int render_perso_rect(t_game *g, t_rect rect, t_img *img)
 	g->pos_pers->y = rect.y;
 	//draw_line(g, g->pos_pers->x + 10, g->pos_pers->y);
 	g->pos_pers -> degree = 0;
-	while (i < 61)
+	while (i < 360)
 	{
-		drawLine_angle(g->pos_pers->x + 10, g->pos_pers->y, g->pos_pers -> degree++ , g);
+		printf("i : %i\n", i);
+		drawLine_angle(g->pos_pers->x, g->pos_pers->y, g->pos_pers -> degree++ , g);
 		if (g->pos_pers -> degree >= 360)
 			break;
 		i++;
--- cub3d/raycasting/math_formula.c
+++ cub3d/raycasting/math_formula.c
@@ -6,7 +6,7 @@
 /*   By: imoumini <imoumini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 19:49:53 by imoumini          #+#    #+#             */
-/*   Updated: 2023/05/25 21:54:37 by imoumini         ###   ########.fr       */
+/*   Updated: 2023/05/26 17:05:37 by imoumini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,14 @@ int draw_line(t_game *g, int x, int y)
 
 void drawLine_angle(int x, int y, double angle,t_game *g)
 {
+    // 30 et 330
+    // if (angle < 0)
+    //     angle = angle * -1;
+    // if (angle > 0)
+    //     angle = 360 - angle;
     double angleInRadians = angle * (PI / 180.0);  // Conversion degrés -> radians
     int newX = x + (int)(g->column *64 * cos(angleInRadians));
-    int newY = y + (int)(g->column *64 * sin(angleInRadians));
+    int newY = y + (int)(g->ligne *64 * sin(angleInRadians));
 
     // Dessiner le trait entre les points (x, y) et (newX, newY)
     int dx = abs(newX - x);
@@ -46,10 +51,12 @@ void drawLine_angle(int x, int y, double angle,t_game *g)
     int sx = (x < newX) ? 1 : -1;
     int sy = (y < newY) ? 1 : -1;
     int err = dx - dy;
-	// printf("c1\n");
-    while ((x != newX || y != newY ) && x < g->column *64 && y < g->ligne*64)
+    while ((x != newX || y != newY ) && (x < g->column * 64 && x > 0) && (y < g->ligne*64 && y > 0))
     {
+        // printf("c2\n");
+	    // printf("c1 x %i y %i\n", x, y);
         img_pix_put(&g->img_mini_map, x, y, RED);
+        // printf("c3\n");
         int err2 = 2 * err;
         if (err2 > -dy)
         {
