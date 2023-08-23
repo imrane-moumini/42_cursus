47693d1b2e3ace8592ff97a6577dbb1b86fb61ed
imrane-moumini <imranemouminipro@gmail.com>
2023-08-23 22:41:33 +0000

    continu ex01 CPP0

--- CPP/CPP0/ex01/main.cpp
+++ CPP/CPP0/ex01/main.cpp
@@ -15,11 +15,15 @@ void fill_contact_info(PhoneBook &phone_book, int i)
 int main(int argc, char *argv[])
 {
     int i;
+    int affiche;
 
+    affiche = 0;
     i = 0;
     std::string input;
     PhoneBook phone_book;
     // au lancement du programme ya une sorte de readline
+    phone_book.tab[0] = Contact();
+    phone_book.tab[0].fill_first_name("Artena");
     while (1)
     {
         std::cout << "Enter a Command\n";
@@ -29,7 +33,8 @@ int main(int argc, char *argv[])
         {
             if (std::strcmp(input, "ADD") == 0)
             {
-                nbr_contact++;
+                if (nbr_contact < 8)
+                    nbr_contact++;
                 if (nbr_contact > 8)
                     i = 0;
                 phone_book.tab[i] = Contact();
@@ -38,10 +43,32 @@ int main(int argc, char *argv[])
             }
             else if (std::strcmp(input, "SEARCH") == 0)
             {
+                // gérer si le répertoir est vide
+                if (std::strcmp(phone_book.tab[0].get_first_name(), "Artena") == 0)
+                    std::cout << "the phoneBook is empty right now please add contact first";
+                else
+                {
+                    while (affiche + 1 < nbr_contact)
+                    {
+                        std::cout << '0' + 1 + affiche << "|";
+                        
+                        // fonction qui compte le nbr de car et qui compte le nbr de car et affiche en fonction de si + ou - de 10
+
+                        // gérer le fait que ça s'affiche à droite
+                        // en fait que ça saffiche a droite c'est simple, je remplis par la fin en mettant d'abord la barre
+                        // et je met le même espace entre l'index barre et les autres mots et je fais commencer chaque text a la barre
+                        // en fait si je défini une taille défini de 10 car et je commence par la fin ça va focément donner cette impression
+                        // en mode la ou ya pas de caractère je met des espaces ça va donner cette impressionb
+                        affiche++;
+                    }
+                }
+                affiche = 0;
+                // gérer affiche un index
                 
             }
             else if (std::strcmp(input, "EXIT") == 0)
             {
+                return (1);
             }
         }
         else
