fddeaa8057ce28a48976f74ce897665dda429a41
imrane-moumini <imranemouminipro@gmail.com>
2023-08-30 16:01:45 +0000

    CPP1 ex04 finish

--- CPP/CPP1/ex04/main.cpp
+++ CPP/CPP1/ex04/main.cpp
@@ -1,43 +1,45 @@
 #include <string>
 #include <iostream>
+#include <cstring>
 #include <fstream>
 
 
 int main(int argc, char *argv[]) {
-    (void)argc;
-    //argv[0] = nom du programme
-    //argv[1] = nom de fichier (filename)
-    //argv[2] = str1
-    //argv[3] = str2
-
-    //ouvrir argv[1]
-    // copier argv[1] dans un nouveau fichier intitulé argv[1].replace 
-    //dans le fichier argv[1].replace remplacer les occurences de str1 par str2
-
-    // gerer les potentielles erreurs
-    // std::string test.replace() est interdit
-    // les fonctions C de manipulation de fichiers sont interdit du coup utiliser celles de CPP
+    if (argc != 4)
+    {
+        std::cout << "wrong number of arguments\n";
+        return (1);
+    } 
     std::string strFile;
     std::ifstream file(argv[1]);
-    // if str
+    if (file.fail())
+    {
+        std::cout << "cannot open the file that your provied please check the filename\n";
+        return (1);
+    }
+    std::string name = argv[1];
+    name.append(".replace");
+    std::ofstream Outfile(name.c_str());
+    if (Outfile.fail())
+    {
+        std::cout << "cannot create the file that your provied please check the filename\n";
+        return (1);
+    }
 
-    //systeme de remplacement dans un fichier
     size_t locationOccur = 0;
     while(std::getline(file, strFile))
     {
-        std::cout << "BEFORE REPLACE STR IS: " << strFile << std::endl;
-        locationOccur = strFile.find("test");
+        locationOccur = strFile.find(argv[2]);
         while (locationOccur != std::string::npos)
         {
-            std::cout << "IM IN LOCATION !!!!!!!\n";
-            // jpeux jouer avec erase et insert
-            strFile.erase(locationOccur,4);
-            strFile.insert(locationOccur, "dackqjsdbnkjqsndolqsk,dqpmks,d");
-            locationOccur = strFile.find("test", locationOccur);
+            strFile.erase(locationOccur,std::strlen(argv[2]));
+            strFile.insert(locationOccur, argv[3]);
+            locationOccur = strFile.find(argv[2], locationOccur);
         }
-        std::cout << "AFTER REPLACE STR IS: " << strFile << std::endl;
+        Outfile << strFile << std::endl;
     }
-    
+    Outfile.close();
+    file.close();
 } 
 
 
