2dd3442d83244e29c74e577c3c16d44ba2519f89
imrane-moumini <imranemouminipro@gmail.com>
2023-11-04 22:21:22 +0000

    add message for deque CPP9 ex02

--- CPP/CPP9/ex02/main.cpp
+++ CPP/CPP9/ex02/main.cpp
@@ -86,7 +86,7 @@ int main(int argc, char *argv[])
 		    vector.push_back(num);
 	    }
 	
-	std::cout << "Before:	";
+	std::cout << "Vector Before:	";
 	for (size_t i = 0; i < vector.size(); i++)
 		std::cout << vector[i] << " ";
 	std::cout << '\n';
@@ -96,17 +96,31 @@ int main(int argc, char *argv[])
 	clock_t end = std::clock();
 	double time_vector = 1000000.0 * (end - start) / CLOCKS_PER_SEC;
 
-	std::cout << "After:	";
+	std::cout << "Vector After:	";
 	for (size_t i = 0; i < vector.size(); i++)
 		std::cout << vector[i] << " ";
 	std::cout << '\n';
 	std::cout << '\n';
 	std::cout << "Time to process a range of " << vector.size() << " elements with std::vector : " << time_vector << " us\n";
 
+
+    std::cout << "---------------------------------\n";
+    std::cout << "deque Before:	";
+	for (size_t i = 0; i < deque.size(); i++)
+		std::cout << deque[i] << " ";
+	std::cout << '\n';
+
+
     clock_t start_deque = std::clock();
 	fordJohnsonSort(deque);
 	clock_t end_deque = std::clock();
 	double time_deque = 1000000.0 * (end_deque - start_deque) / CLOCKS_PER_SEC;
+
+    std::cout << "deque After:	";
+	for (size_t i = 0; i < deque.size(); i++)
+		std::cout << deque[i] << " ";
+	std::cout << '\n';
+	std::cout << '\n';
 
     std::cout << "Time to process a range of " << vector.size() << " elements with std::deque : " << time_deque << " us\n";
 
