335dce4979a450cfe9cbfd52ad626cbe55e356fd
imrane-moumini <imranemouminipro@gmail.com>
2023-11-04 00:08:02 +0000

    check error of car, il reste les erreurs de mauvaise ecriture du reverse polish

--- CPP/CPP9/ex01/main.cpp
+++ CPP/CPP9/ex01/main.cpp
@@ -1,4 +1,24 @@
 #include "RPN.hpp"
+int checkError(char *argv[])
+{
+    int i = 0;
+
+    while (argv[1][i])
+    {
+        // si pas chiffres ou espace ou signe
+        if (!(argv[1][i] >= '0' && argv[1][i] <= '9'))
+        {
+            if (argv[1][i] != ' ')
+            {
+                if (argv[1][i] != '+' && argv[1][i] != '*' && argv[1][i] != '-' && argv[1][i] != '/')
+                    return (1);
+            }
+        }
+
+        i++;
+    }
+    return (0);
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +34,11 @@ int main(int argc, char *argv[])
     int result;
     std::string car;
     int i = 0;
-
+    if (checkError(argv))
+    {
+        std::cout << "Error\n";
+        return (1);
+    }
     while (argv[1][i])
     {
         switch (argv[1][i])
