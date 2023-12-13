015b84daf0e845fb765f195a5b4a08d7183be17a
imrane-moumini <imranemouminipro@gmail.com>
2023-12-13 16:32:30 +0000

    few changes

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#set -eux
+
 
 service mariadb start;
 
-# log into MariaDB as root and create database and the user
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
@@ -11,8 +10,4 @@ mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mariadb -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
-#mysqladmin -u root shutdown
 exec mysqld_safe
-
-#print status
-echo "MariaDB database and user were created successfully! "
--- inception/srcs/requirements/wordpress/tools/auto_config.sh
+++ inception/srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#set -eux
 
 if [ ! -d /run/php ]; then
 	mkdir /run/php;
@@ -23,24 +22,13 @@ wp core install	--allow-root \
 			--admin_password=${ADMIN_PASSWORD} \
 			--admin_email=${ADMIN_EMAIL};
 
-# wp option update home https://${DOMAIN_NAME} --allow-root
-# wp option update siteurl https://${DOMAIN_NAME} --allow-root
 
 wp user create		--allow-root \
 			${USER1_LOGIN} ${USER1_MAIL} \
 			--user_pass=${USER1_PASS} ;
-			# --role=author \
 
 wp cache flush --allow-root
 
-# wp plugin install contact-form-7 --activate
-
-# wp language core install en_US --activate
-
-# wp theme delete twentynineteen twentytwenty
-# wp plugin delete hello
-
-# wp rewrite structure '/%postname%/'
 
 fi
 
