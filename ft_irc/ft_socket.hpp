4f4c519948d49f08bbd22c3fffb4e895e543cdac
imrane-moumini <imranemouminipro@gmail.com>
2024-01-14 20:25:33 +0000

    add select, fonctionne pour un irssi, a tester avec plusieurs

--- ft_irc/ft_socket.hpp
+++ ft_irc/ft_socket.hpp
@@ -12,6 +12,9 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/time.h> 
+#include <sys/types.h>  
 #include <unistd.h>
 
 // socket function
--- ft_irc/main.cpp
+++ ft_irc/main.cpp
@@ -7,25 +7,50 @@ int main(int argc, char *argv[])
     int sockFd;
     int newsockFd;
     int portNb;
-    
+    fd_set current_sockets, ready_sockets;
     (void)argc;
     (void) argv;
     struct sockaddr_in sockStructServ;
     struct sockaddr_in sockStructClient;
 
     portNb = 6697;
+    //initialize my current set of sockets
     
     /* Initialize socket structure */
     sockFd = i_setup_socket(&sockStructServ, portNb);
-    
+    FD_ZERO(&current_sockets);
+    FD_SET(sockFd, &current_sockets);
+
     while (true)
     {
+        // save current sockets because select is destructive
+        ready_sockets = current_sockets;
+        if (select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0)
+        {
+            perror("select error");
+            exit(1);
+        }
+
+        for (int i=0; i < FD_SETSIZE; i++)
+        {
+            if (FD_ISSET(i, &ready_sockets))
+            {
+                if (i == sockFd){
+                    // this is a new connexion
+                    // accept connexion
+                    newsockFd = i_accept_connexion(sockFd, &sockStructClient);
+                    FD_SET(newsockFd, &current_sockets);
+                } else {
+                    //handle connexion
+                    i_handle_connexion(newsockFd);
+                    FD_CLR(i, &current_sockets);
+                }
+            }
+        }
         
-        // accept connexion
-        newsockFd = i_accept_connexion(sockFd, &sockStructClient);
         
-        //handle connexion
-       i_handle_connexion(newsockFd);
+        
+        
     }
     std::cout << "YES \n";
 }
